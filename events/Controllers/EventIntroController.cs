233a16ae431d2911752311cbf21d33b6dfbef9d3
Vincent Yang <mscsharp@gmail.com>
2017-02-12 23:45:27 +1100

    Completed Module 10 exercises

---
+++ events/Controllers/EventIntroController.cs
@@ -0,0 +1,40 @@
+﻿using events.Models;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Web.UI.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace events.Controllers
+{
+    public class EventIntroController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(CreateModel());
+        }
+
+        private static EventIntro CreateModel()
+        {
+            var item = RenderingContext.Current.ContextItem;
+
+            var eventIntro = new EventIntro
+            {
+                Heading = new System.Web.HtmlString(FieldRenderer.Render(item, "ContentHeading")),
+                EventImage = new System.Web.HtmlString(FieldRenderer.Render(item, "Event Image", "mw=400")),
+                Highlights = new System.Web.HtmlString(FieldRenderer.Render(item, "Highlights")),
+                Intro = new System.Web.HtmlString(FieldRenderer.Render(item, "ContentIntro")),
+                StartDate = new System.Web.HtmlString(FieldRenderer.Render(item, "Start Date")),
+                Duration = new System.Web.HtmlString(FieldRenderer.Render(item, "Duration")),
+                Difficulity = new System.Web.HtmlString(FieldRenderer.Render(item, "Difficulity"))
+
+            };
+
+
+            return eventIntro;
+        }
+    }
+}
---
+++ events/Models/EventIntro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace events.Models
+{
+    public class EventIntro
+    {
+        public EventIntro()
+        {
+
+        }
+
+        public HtmlString Heading { get; set; }
+        public HtmlString Intro { get; set; }
+        public HtmlString Body { get; set; }
+        public HtmlString EventImage { get; set; }
+        public HtmlString Highlights { get; set; }
+        public HtmlString StartDate { get; set; }
+        public HtmlString Duration { get; set; }
+        public HtmlString Difficulity { get; set; }
+    }
+}
